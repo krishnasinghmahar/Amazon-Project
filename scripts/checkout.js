0a19231e2405d1d66676fafd2355d4a172ed77a7
Krishna Singh Mahar <krishnasinghmaharksm@gmail.com>
2024-06-05 21:43:49 +0530

    convert  loadProduct from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,17 +1,25 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 
-new Promise((resolve)=>{
-  loadProducts(()=>{
-    resolve();
-  })
-}).then(()=>{
+loadProductsFetch().then(() => {
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 })
+
+
+// new Promise((resolve) => {
+//   loadProducts(() => {
+//     resolve();
+//   })
+// }).then(() => {
+//   renderCheckoutHeader();
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// })
+
 
 // loadProducts(() => {
 //   renderCheckoutHeader();
